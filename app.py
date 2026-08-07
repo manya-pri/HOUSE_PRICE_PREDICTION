f4ae7a174598b4c867e1465d7340354b10a0f264
manyashree01 <manyashree553@gmail.com>
2026-08-07 23:44:27 +0530

    Update house price prediction app

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ if st.button("Predict Price"):
 
     prediction = model.predict(features)
 
-    st.success(f"Predicted House Price: ₹ {prediction[0]:,.2f}")
+    st.success(f"Predicted House Price: ₹ {prediction[0]:,.2f}")2wqsq
